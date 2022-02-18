c3030da51022f5bbedf684a141b363e93e433c98
Peter Teuben <teuben@gmail.com>
2022-02-17 20:41:01 -0500

    make small table the default

--- query_alma.py
+++ query_alma.py
@@ -11,21 +11,24 @@ q1 = "SELECT * FROM ivoa.obscore"
 q2 = "SELECT * FROM ivoa.obscore WHERE target_name = 'NGC3504'"
 q3 = "NGC3504"
 
-query = q1
+query = q2
+
+url = "https://almascience.nrao.edu/tap"
+url = "https://almascience.eso.org/tap"
 
 if mode == 1:
-    print("pyvo",query)
+    print("pyvo: ",query)
     
     import pyvo
 
-    service = pyvo.dal.TAPService("https://almascience.nrao.edu/tap") 
+    service = pyvo.dal.TAPService(url)
     r1 = service.search(query)
     p1 = r1.to_table().to_pandas()
     p1.to_pickle('alma.pickle')
     print('Wrote alma.pickle')
     
 elif mode == 2:
-    print("aq query_tap",query)
+    print("astroquery.alma.query_tap:",query)
     
     from astroquery.alma import Alma
 
@@ -36,7 +39,7 @@ elif mode == 2:
     
 
 elif mode == 3:
-    print("aq query_object",q3) 
+    print("astroquery.alma.query_object",q3) 
 
     from astroquery.alma import Alma
 
@@ -50,4 +53,4 @@ elif mode == 3:
 import pickle
 
 a = pickle.load(open('alma.pickle','rb'))
-print("read pickly into",type(a),a)
+print("read pickle into",type(a),a)
