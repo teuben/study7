27946c1b83d9bef50ecfc6e916d7b9a1f4255a63
Marc Pound <22331890+mpound@users.noreply.github.com>
2022-02-18 19:40:50 -0500

    update for new admit/core.py

--- check2.py
+++ check2.py
@@ -2,20 +2,18 @@
 #
 
 from astroquery.admit import ADMIT
-from astroquery.admit.core import _gen_sql
 
 if True:
     import pickle
     a = pickle.load(open('alma.pickle','rb'))
 
 a = ADMIT()
-a.query()
 a.check()
 payload = { "spw": 123, 
             "velocity": 88.0,
             "transition": 'CO*|H2O',
           }
-print("PAYLOAD SQL",_gen_sql(payload))
+a.query(payload)
 
 
 if False:
