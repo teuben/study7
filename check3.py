90ce4a783a674ee3a7bb5a076a7fc0cafa7fc843
Marc Pound <22331890+mpound@users.noreply.github.com>
2022-02-23 12:03:34 -0500

    source_snr

--- check3.py
+++ check3.py
@@ -13,14 +13,14 @@ payload = {
             "velocity": 88.0,
             "formula": 'CO*|H2O',
             "pub_abstract": "*YSO* | *young stellar object*",
-            "snr": ">3",
+            "source_snr": ">3",
             "spatial_resolution": "<10",
           }
 
 p1 = { 
     "source_name_alma": "NGC3504",
     "formula": 'CO',
-    "snr": ">3",
+    "source_snr": ">3",
 }
 
 r1 = a.query(**p1)
