2d950aa129b72e4340ded965bb38479c8bde4b5e
Peter Teuben <teuben@gmail.com>
2022-02-27 20:45:49 -0500

    upd with cli

--- check4.py
+++ check4.py
@@ -1,10 +1,19 @@
 #! /usr/bin/env python
 #
+#    summarize an admit.db database
+#
+
+import sys
 
 from astroquery.admit import ADMIT
 
+db = 'admit.db'
 
-a = ADMIT('admit_maps.db')
+if len(sys.argv) > 1:
+    db = sys.argv[1]
+
+
+a = ADMIT(db)
 a.check()
 
 p = a.query(nchan='>0')
@@ -16,3 +25,17 @@ print(len(p))
 f=p['formula'].unique()
 print(len(f),'unique lines: ',f)
 
+# number of spectral cubes and cont maps
+pl=a.query(nchan='>2')
+pc=a.query(nchan=1)
+
+#  various orphans: lines with no sources, sources with no lines
+ps0=a.query(nchan='>2',nlines='>1',nsources=0)
+ps1=a.query(nchan='>2',nlines='>1')
+pl0=a.query(nchan='>2',nlines=0,nsources='>1')
+
+print("Continuum maps:                      ",len(pc))
+print('Spectral Line Cube:                  ',len(pl))
+print('  w/ Line detections:                ',len(ps1))
+print('  w/ Line detections with no sources:',len(ps0))
+print('  w/ Source detections with no lines:',len(pl0))
